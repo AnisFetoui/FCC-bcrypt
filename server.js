d7d62d5f395c131dab555b2e6a276793239db688
AnisFetoui <anis.fetoui@esprit.tn>
2023-12-07 18:10:17 +0100

    corrected the version of add hash and hash compare method functions

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const someOtherPlaintextPassword = 'pass123';
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
-    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
         console.log(res);
       });
   });
