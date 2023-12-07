6e7650efb0f138093f31a4551865b0bc9bd51cdd
AnisFetoui <anis.fetoui@esprit.tn>
2023-12-07 17:33:54 +0100

    Understand BCrypt Hashes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
